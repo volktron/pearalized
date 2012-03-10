24e138d4ada2074bbc73e3cd5cd463e8b3849673
Henrik Volckmer <danorux@gmail.com>
2012-03-09 21:20:59 -0500

    Config file.

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+/*	Config.php
+ *	Pearless Configuration File
+ *	@author Henrik Volckmer
+ */
+
+// This is the document root, normally one level above htdocs.
+$doc_root = $_SERVER["DOCUMENT_ROOT"];
+
+// htdocs directory
+$htdocs = $doc_root."/htdocs";
+
+// CSS path relative 
+$css_path = $htdocs."/css";
+
+// Pearless path
+$pearless_path = $doc_root."/pearless";
+
+// Database configuration
+$db_type = "mysql";
+$db_host = "";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
+
+// Memcached (optional)
+$memcache_host = "localhost";
+$memcache_port = 11211;
+
+// Include database functions
+require_once($doc_root."/".$include_path."/.config.".$db_type.".php");
+
+?>
