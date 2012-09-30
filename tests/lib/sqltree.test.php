394e04b357e8be59375161508ec77e85db24bf9d
Henrik Volckmer <danorux@gmail.com>
2012-09-30 16:04:31 -0400

    Updated tests to work with new file structure.

--- tests/lib/sqltree.test.php
+++ tests/lib/sqltree.test.php
@@ -1,24 +1,32 @@
 <?php
-
+/*
+ * sqltree.test.php
+ * @author Henrik Volckmer
+ */
+ 
+/**
+ * @backupGlobals disabled
+ * @backupStaticAttributes disabled
+ */
 require_once("config.php");
 
 class SQLTreeTest extends PHPUnit_Framework_TestCase
 {
 	public function test_tokens()
 	{
-		$tokens = p("lib > SQLTree")->sql("SELECT a FROM b")->tokens();		
+		$tokens = p("lib > sqltree > SQLTree")->sql("SELECT a FROM b")->tokens();		
 		$this->assertTrue(is_array($tokens) && sizeof($tokens) == 4);
 		
-		$tokens = p("lib > SQLTree")->sql("SELECT a,b,c FROM d")->tokens();		
+		$tokens = p("lib > sqltree > SQLTree")->sql("SELECT a,b,c FROM d")->tokens();		
 		$this->assertTrue(is_array($tokens) && sizeof($tokens) == 8);
 		
-		$tokens = p("lib > SQLTree")->sql("SELECT 'a b' as c FROM d")->tokens();		
+		$tokens = p("lib > sqltree > SQLTree")->sql("SELECT 'a b' as c FROM d")->tokens();		
 		$this->assertTrue(is_array($tokens) && sizeof($tokens) == 6);
 
-		$tokens = p("lib > SQLTree")->sql("SELECT \"a ' b\" as c FROM d")->tokens();		
+		$tokens = p("lib > sqltree > SQLTree")->sql("SELECT \"a ' b\" as c FROM d")->tokens();		
 		$this->assertTrue(is_array($tokens) && sizeof($tokens) == 6);
 
-		$tokens = p("lib > SQLTree")->sql("SELECT (a b) as c FROM d")->tokens();		
+		$tokens = p("lib > sqltree > SQLTree")->sql("SELECT (a b) as c FROM d")->tokens();		
 		$this->assertTrue(is_array($tokens) && sizeof($tokens) == 9);
 	}
 	
@@ -30,7 +38,7 @@ class SQLTreeTest extends PHPUnit_Framework_TestCase
 		};
 	
 		// The first assertion is to verify the simplest working implementation
-		$tree = p("lib > SQLTree")->sql("SELECT a FROM b");
+		$tree = p("lib > sqltree > SQLTree")->sql("SELECT a FROM b");
 		$tree->callbacks(["select_values" => $function]);
 		
 		//print_r($tree->tokens());
